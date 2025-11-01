b3ac0f49be8da9abac1764c46bd289689fde2ab4
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:32:57 +0530

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.java
@@ -0,0 +1,9 @@
+// Last updated: 11/1/2025, 9:32:56 PM
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++){
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
