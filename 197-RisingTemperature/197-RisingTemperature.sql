a53b12af53a57c002bfc3da108d38aa7542f7c4b
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:34:17 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,26 @@
+-- Last updated: 11/1/2025, 9:34:16 PM
+-- Write your PostgreSQL query statement below
+
+-- Using self join
+-- SELECT w1.id
+-- FROM Weather w1
+-- INNER JOIN Weather w2
+-- ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+-- WHERE w1.temperature > w2.temperature;
+
+-- Using lag
+WITH rising AS (
+  SELECT id,
+         recordDate,
+         temperature,
+         LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+         LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp
+    FROM Weather
+)
+SELECT id
+FROM rising
+WHERE recordDate = prev_date + INTERVAL '1 day'
+  AND temperature > prev_temp;
+
+
+
