a6f736092a9edf2187940445c12450107cac55af
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:33:04 +0530

    Create 1338-QueriesQualityAndPercentage

---
+++ 1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,8 @@
+-- Last updated: 11/1/2025, 9:33:03 PM
+-- Write your PostgreSQL query statement below
+SELECT query_name, 
+    ROUND(AVG(rating * 1.0 / position)::NUMERIC, 2) AS quality,
+    ROUND(SUM((rating < 3)::INT) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+WHERE 1 IS NOT NULL
+GROUP BY 1;
