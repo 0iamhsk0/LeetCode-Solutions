d307ee5d9a192ed58528bff68e36967f175f6ab1
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:34:14 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.py
@@ -0,0 +1,29 @@
+# Last updated: 11/1/2025, 9:34:13 PM
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+
+            if n == 1:
+                return True
+        
+        return False
+
+
+    def sumOfSquares(self, n:int) -> int:
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            output += digit
+            n = n // 10
+        
+        return output
+        
+
+
+        
