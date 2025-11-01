074f6bec59e8c3b8e4323a1e65b8beea5bb3d70e
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:33:42 +0530

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.py
@@ -0,0 +1,11 @@
+# Last updated: 11/1/2025, 9:33:40 PM
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        
+        return sp == len(s)
