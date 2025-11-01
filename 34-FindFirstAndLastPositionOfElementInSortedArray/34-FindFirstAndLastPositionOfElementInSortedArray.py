1a640b5cb680b9c6e4352f8d823ba1e3e5f06022
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:35:11 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.py
@@ -0,0 +1,30 @@
+# Last updated: 11/1/2025, 9:35:10 PM
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        def binary_search(nums, target, is_searching_left):
+            left = 0
+            right = len(nums) - 1
+            idx = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] > target:
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    idx = mid
+                    if is_searching_left:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+            
+            return idx
+        
+        left = binary_search(nums, target, True)
+        right = binary_search(nums, target, False)
+        
+        return [left, right]
+            
