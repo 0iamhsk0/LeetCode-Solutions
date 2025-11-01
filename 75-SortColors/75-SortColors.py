ce7108708650f960eff9a276ad4d6ddc68baa8ef
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-11-01 21:34:58 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,9 @@
+# Last updated: 11/1/2025, 9:34:57 PM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+
+        """
+        return nums.sort()
+        
