cfe8c4ec68ddc1ed8d59067325ea6650ad9c6ee4
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2026-07-30 11:46:09 +0530

    Time: 406 ms (14.29%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,35 @@
+-- SELECT 
+--     m.employee_id,
+--     m.name,
+--     COUNT(e.employee_id) AS reports_count,
+--     ROUND(AVG(e.age)) AS average_age
+-- FROM employees e
+-- JOIN employees m
+--     ON e.reports_to = m.employee_id
+-- GROUP BY m.employee_id, m.name
+-- ORDER BY m.employee_id;
+
+WITH emp_mgr_table AS (
+    SELECT 
+        e2.employee_id AS manager_id,
+        e2.name AS manager_name,
+        e1.employee_id AS sub_id,
+        e1.age AS sub_age
+    FROM Employees AS e1
+    JOIN Employees AS e2
+        ON e1.reports_to = e2.employee_id
+)
 SELECT 
-    m.employee_id,
-    m.name,
-    COUNT(e.employee_id) AS reports_count,
-    ROUND(AVG(e.age)) AS average_age
-FROM employees e
-JOIN employees m
-    ON e.reports_to = m.employee_id
-GROUP BY m.employee_id, m.name
-ORDER BY m.employee_id;
+    manager_id AS employee_id,
+    manager_name AS name, 
+    COUNT(sub_id) AS reports_count,
+    ROUND(AVG(sub_age)) AS average_age
+FROM emp_mgr_table 
+GROUP BY 1, 2
+ORDER BY 1;
+
+
+
+
+
+
